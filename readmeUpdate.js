ab44dbb1711984181998a6c3c6d7e4b76b1affad
daunload <daun4799@gmail.com>
2025-12-07 22:58:58 +0900

    readmeupdate.js 수정

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -2,20 +2,10 @@ import { writeFileSync } from "node:fs";
 import Parser from "rss-parser";
 
 const staticContent = `
-<div align="center">
 
-👋 Hi, there! I'm **Daun Seo**
+#👋 Hi, there! I'm **Daun Seo**
 
-</div>
-<div align="center">
-<h2 style="border-bottom: 1px solid #d8dee4; color: #282d33;">
-    Blog Posts
-</h2>
-<div align="center">
-    <a href="https://dev-district.tistory.com">
-    <img src="https://img.shields.io/badge/Tistory-000000?style=for-the-badge&logo=Tistory&logoColor=white&link=https://dev-district.tistory.com">
-    </a>
-</div>
+## 📕 Latest Blog Posts
 `;
 
 const parser = new Parser({
